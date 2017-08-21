f9ae3500aad3d05b2c9dd75c23945b48c0114021
Afy <afispikes@gmail.com>
2017-08-21 17:12:46 +0800

    Changes made by Afy

---
+++ UES/Controllers/CreateUpgrownEmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UES.Models.Account;
+
+namespace UES.Controllers
+{
+    public class CreateUpgrownEmployeeController : Controller
+    {
+        // GET: CreateUpgrownEmployee
+        public ActionResult CreateUpgrownEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult CreateUpgrownEmployee(CreateUpgrownEmployeeModel createUpgrownEmployeeModel)
+        {
+         
+            //Check against model data annotation
+            if (ModelState.IsValid)
+            {
+                //Can now be stored in database
+            }
+            else
+            {
+                //at least 1 data annotation failed in Model.DemoUser
+            }
+
+            return View(createUpgrownEmployeeModel);
+        }
+    }
+}
