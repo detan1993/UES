652af2c031f4ab4f5485e1b1a7f1107b08309bc6
DESKTOP-ML0KNPO\David <E0002311@u.nus.edu>
2017-08-21 14:38:01 +0800

    Add "CreateRestaurantModel".

---
+++ UES/Models/CreateRestaurantModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace UES.Models
+{
+    public class CreateRestaurantModel
+    {
+        [Required]
+        public string restaurantName { get; set; }
+
+        [Required]
+        public string personInCharge { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string personInChargeEmail { get; set; }
+
+        [Required]
+        public int personInChargeContact { get; set; }
+
+        [Required]
+        public string address { get; set; }
+
+        [Required]
+        public string region { get; set; }
+    }
+}
